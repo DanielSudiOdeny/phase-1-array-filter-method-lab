e698bfb24e120fa2dcc3ea7daf10c689cff8a33f
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-19 23:03:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const findMatching = function (drivers, string) {
+  const matchingList = drivers.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+  return matchingList;
+};
+
+const fuzzyMatch = function (drivers, string) {
+  const providedLettersMatch = drivers.filter(function (driver) {
+    return driver.slice(0, 2) === string;
+  });
+
+  return providedLettersMatch;
+};
+
+const matchName = function (drivers, string) {
+  const matchingNames = drivers.filter(function (driver) {
+    return driver["name"] === string;
+  });
+
+  return matchingNames;
+};
